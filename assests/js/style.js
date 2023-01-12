dd4b05e113b1fca370ea9492f243d22c3fa30dc2
Fayven <fayven.amelga@gmail.com>
2023-01-12 00:18:53 -0500

    Add getWeather function

--- assests/js/style.js
+++ assests/js/style.js
@@ -16,12 +16,12 @@ var currentdayEl = document.getElementsByClassName("current-date");
 
 function search(event) {
   event.preventDefault;
-  getWeather();
+  getForecast();
 }
 //   console.log(event);
 //   console.log(cityInput.value);
 //   console.log(typeof cityInput.value);
-function getWeather() {
+function getForecast() {
   var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput.value + "&appid=" + apiKey + "&units=imperial";
   console.log(requestUrl);
   // var api = apiBase+cityInput+apiKey;
@@ -33,12 +33,24 @@ function getWeather() {
       console.log(data);
     //   currentdayEl.textContent = data;
     });
-
+    getWeather();
   // run a fetch with the cityInput.value
   // once fetch response is back call function to display the data
 }
 
-// function getWeather(cityName) {
+function getWeather(cityName) {
+    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" +cityInput.value + "&appid=" + apiKey + "&units=imperial";
+    console.log(requestUrl);
+    fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    //   currentdayEl.textContent = data;
+    });
+}
+
 //     // do something with cityName
 //     var weatherData = fetch('getMyWeather');
 //     step2(weatherData, cityName);
