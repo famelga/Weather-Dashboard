c1fca705cfc034ca0ad8d108b7d5774ff37271fd
Fayven <fayven.amelga@gmail.com>
2023-01-12 01:08:36 -0500

    Append weather function var to html

--- assests/js/style.js
+++ assests/js/style.js
@@ -4,6 +4,8 @@ var apiKey = "189579eeb4103c71fbfaccf9d7f42125";
 var cityInput = document.getElementById("search-input");
 var btnEl = document.querySelector(".btn");
 var currentdayEl = document.getElementsByClassName("current-date");
+// var todayEl = document.getElementById("today");
+var fiveEl = document.getElementById("five");
 
 function search(event) {
   event.preventDefault;
@@ -41,18 +43,26 @@ function getWeather(cityName) {
 
 // current weather data:
 function currentWeatherData(data) {
+    var todayEl = document.getElementById("today");
     var nameEl = document.createElement("h2");
     nameEl.textContent = data.name;
+    // todayEl.appendChild(nameEl);
+    todayEl.appendChild(nameEl);
     var dateEl = document.createElement("h3");
     dateEl.textContent = data.dt;
+    todayEl.appendChild(dateEl);
     var iconEl = document.createElement("img");
     iconEl.textContent = data.weather[0].icon;
+    todayEl.appendChild(iconEl);
     var tempEl = document.createElement("h5");
     tempEl.textContent = data.main.temp;
+    todayEl.appendChild(tempEl);
     var windEl = document.createElement("h5");
     windEl.textContent = data.wind.speed;
+    todayEl.appendChild(windEl);
     var humidityEl = document.createElement("h5");
     humidityEl.textContent = data.main.humidity;
+    todayEl.appendChild(humidityEl);
 } 
 
 function forecastData(data) {
